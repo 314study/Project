c1480c83deae69dcb2ad1baf9657449bce2c6858
kainyogi <kainyogi@hawaii.edu>
2018-11-07 14:42:58 -1000

    Commit Base

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+        <div className="inverted-section">
+          <div className="ui center aligned container">
+            <p className="Nunito-font font-large">
+              <span className="font-bold font-color-white">Welcome to</span>
+              <span className="font-bold font-color-green"> LO</span>
+              <span className="font-color-green">GO</span>
+              <p className="font-small font-color-grey">
+                <span className="font-bold font-color-lightgrey">P</span>eople
+                <span className="font-bold font-color-lightgrey"> H</span>ave
+                <span className="font-bold font-color-lightgrey"> Q</span>uestions,
+                <span className="font-bold font-color-lightgrey"> S</span>o
+                <span className="font-bold font-color-lightgrey"> H</span>elp
+                <span className="font-bold font-color-lightgrey"> T</span>hem
+                <span className="font-bold font-color-lightgrey"> P</span>lease.
+              </p>
+            </p>
+            <Grid container stackable centered columns={3}>
+              <Grid.Column centered>
+                <p className="font-color-white">CARD</p>
+              </Grid.Column>
+              <Grid.Column>
+                <p className="font-color-white">CARD</p>
+              </Grid.Column>
+              <Grid.Column>
+                <p className="font-color-white">CARD</p>
+              </Grid.Column>
+            </Grid>
+          </div>
+        </div>
     );
   }
 }

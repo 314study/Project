cc8fc6e52932d840575e233b940c47e3dcd6076c
Aaron Geronimo <42796623+aaronjgeronimo@users.noreply.github.com>
2018-11-07 15:49:50 -1000

    Update footer/ Add grid container

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
+import { Grid, List } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
     return (
         <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              PHQSHTP inc llc est 2018 ltd <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822
-          </div>
+          <Grid textAlign='center' container>
+            <Grid.Column width={3}>
+              <List> Useful links </List>
+              <hr/>
+              <List.Item> Khan Academy </List.Item>
+              <List.Item> The text book bro </List.Item>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <List>We like ratings!</List>
+              <hr/>
+              <List.Item>A Mentor</List.Item>
+              <List.Item>A Student</List.Item>
+              <List.Item>This app</List.Item>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <List>Support Us</List>
+              <hr/>
+              <List.Item>PayPal</List.Item>
+              <List.Item>GoFund Me</List.Item>
+              <List.Item>PLz HAlp, need MoNeeyzz</List.Item>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <List>About Us</List>
+              <hr/>
+              <List.Item>
+                PHQSHTP inc llc est 2018 ltd <br />
+                University of Hawaii<br />
+                Honolulu, HI 96822
+              </List.Item>
+            </Grid.Column>
+          </Grid>
         </footer>
     );
   }

3309c9ecddb07b65fdb01ddda7314597c264e2e8
Aaron Geronimo <42796623+aaronjgeronimo@users.noreply.github.com>
2018-11-07 20:36:53 -1000

    Addded Logo/ Updated styling on footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -4,40 +4,43 @@ import { Grid, List } from 'semantic-ui-react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
+    const divStyle = { paddingTop: '15px' };
     return (
-        <footer>
-          <Grid textAlign='center' container>
-            <Grid.Column width={3}>
-              <List> Social Media </List>
-              <hr/>
-              <List.Item> Facebook </List.Item>
-              <List.Item> Instagram </List.Item>
-              <List.Item> Twitter </List.Item>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <List>Useful Links</List>
-              <hr/>
-              <List.Item>Khan Academy</List.Item>
-              <List.Item>Chegg</List.Item>
-              <List.Item>Stack Overflow</List.Item>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <List>Support Us</List>
-              <hr/>
-              <List.Item>PayPal</List.Item>
-              <List.Item>GoFund Me</List.Item>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <List>About Us</List>
-              <hr/>
-              <List.Item>
-                PHQSHTP inc. llc. co. LtD. est 2018 <br />
-                University of Hawaii<br />
-                Honolulu, HI 96822
-              </List.Item>
-            </Grid.Column>
-          </Grid>
-        </footer>
+        <div style={divStyle}>
+          <footer>
+            <Grid textAlign='center' container>
+              <Grid.Column width={3}>
+                <List> Social Media </List>
+                <hr/>
+                <List.Item> Facebook </List.Item>
+                <List.Item> Instagram </List.Item>
+                <List.Item> Twitter </List.Item>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <List>Useful Links</List>
+                <hr/>
+                <List.Item>Khan Academy</List.Item>
+                <List.Item>Chegg</List.Item>
+                <List.Item>Stack Overflow</List.Item>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <List>Support Us</List>
+                <hr/>
+                <List.Item>PayPal</List.Item>
+                <List.Item>GoFund Me</List.Item>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <List>About Us</List>
+                <hr/>
+                <List.Item>
+                  PHQSHTP inc. llc. co. LtD. est 2018 <br />
+                  University of Hawaii<br />
+                  Honolulu, HI 96822
+                </List.Item>
+              </Grid.Column>
+            </Grid>
+          </footer>
+        </div>
     );
   }
 }

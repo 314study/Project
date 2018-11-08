dfec02fb1326568dd3fd1ee7f55e006d56ba9ab2
kainyogi <kainyogi@hawaii.edu>
2018-11-07 22:06:11 -1000

    Edited Landing
    added a div wrapper
    Editted branch name
    CSS/Middle -> Issue 9

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,33 +5,38 @@ import { Grid, Image, Header } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
-        <div className="inverted-section">
-          <div className="ui center aligned container">
-            <p className="Nunito-font font-large">
-              <span className="font-bold font-color-white">Welcome to</span>
-              <span className="font-bold font-color-green"> LO</span>
-              <span className="font-color-green">GO</span>
-              <p className="font-small font-color-grey">
-                <span className="font-bold font-color-lightgrey">P</span>eople
-                <span className="font-bold font-color-lightgrey"> H</span>ave
-                <span className="font-bold font-color-lightgrey"> Q</span>uestions,
-                <span className="font-bold font-color-lightgrey"> S</span>o
-                <span className="font-bold font-color-lightgrey"> H</span>elp
-                <span className="font-bold font-color-lightgrey"> T</span>hem
-                <span className="font-bold font-color-lightgrey"> P</span>lease.
+        <div>
+          <div className="inverted-section">
+            <div className="ui center aligned container">
+              <p className="Nunito-font font-large">
+                <span className="font-bold font-color-white">Welcome to</span>
+                <span className="font-bold font-color-green"> Stud</span>
+                <span className="font-color-green">Budd</span>
+                <p className="font-small font-color-grey">
+                  <span className="font-bold font-color-lightgrey">P</span>eople
+                  <span className="font-bold font-color-lightgrey"> H</span>ave
+                  <span className="font-bold font-color-lightgrey"> Q</span>uestions,
+                  <span className="font-bold font-color-lightgrey"> S</span>o
+                  <span className="font-bold font-color-lightgrey"> H</span>elp
+                  <span className="font-bold font-color-lightgrey"> T</span>hem
+                  <span className="font-bold font-color-lightgrey"> P</span>lease.
+                </p>
               </p>
-            </p>
-            <Grid container stackable columns={3}>
-              <Grid.Column>
-                <p className="font-color-white">CARD</p>
-              </Grid.Column>
-              <Grid.Column>
-                <p className="font-color-white">CARD</p>
-              </Grid.Column>
-              <Grid.Column>
-                <p className="font-color-white">CARD</p>
-              </Grid.Column>
-            </Grid>
+              <Grid container stackable columns={3}>
+                <Grid.Column>
+                  <p className="font-color-white">CARD</p>
+                </Grid.Column>
+                <Grid.Column>
+                  <p className="font-color-white">CARD</p>
+                </Grid.Column>
+                <Grid.Column>
+                  <p className="font-color-white">CARD</p>
+                </Grid.Column>
+              </Grid>
+            </div>
+          </div>
+          <div>
+            
           </div>
         </div>
     );

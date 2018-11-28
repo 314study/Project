b747ca2c14effdfde69ba51fb3dd7ad360828916
Neezcha Dinman <42785204+neezcha@users.noreply.github.com>
2018-11-28 11:05:38 -1000

    class list menu

--- app/imports/ui/pages/ClassList.jsx
+++ app/imports/ui/pages/ClassList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, List, Header, Loader, Icon, Segment } from 'semantic-ui-react';
+import { Menu, Container, Loader, Dropdown } from 'semantic-ui-react';
 import { Mentors } from '/imports/api/mentor/mentor';
-import MentorCard from '/imports/ui/components/MentorCard';
+// import MentorCard from '/imports/ui/components/MentorCard';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import classSubMenu from 'ui/components/classSubMenu'
+import classSubMenu from 'ui/components/classSubMenu';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ClassList extends React.Component {
@@ -17,40 +17,79 @@ class ClassList extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
+    const { activeItem } = this.state || {};
     return (
-        <div>
-          <classSubMenu/>
-        </div>
-        /*
-        11/15/18 - Jeff
-        Implementing layout for mentor list display
-        Nothing Changed yet
-        Only changed mentor references and imported Mentors schema from /imports/api/mentor/mentor
-        {this.props.mentors.map(mentor => <MentorItemAdmin key={mentor._id} mentor={mentor} />)}
-        */
         <Container>
-          <Header as="h1" textAlign="center">ClassList</Header>
-          <Segment.Group>
-            <Segment>
-              <Header as='h1'><Icon name='user'/>Example Name</Header>
-              <Header as='h2'><Icon name='book'/>Subjects</Header>
-              <List bulleted relaxed>
-                <List.Item>Class 1</List.Item>
-                <List.Item>Class 2</List.Item>
-                <List.Item>Class 3</List.Item>
-              </List>
-            </Segment>
-            <Segment>
-              <Header as='h1'><Icon name='user'/>Example Name</Header>
-              <Header as='h2'><Icon name='book'/>Subjects</Header>
-              <List bulleted relaxed>
-                <List.Item>Class 1</List.Item>
-                <List.Item>Class 2</List.Item>
-                <List.Item>Class 3</List.Item>
-              </List>
-            </Segment>
-            {this.props.mentors.map(mentor => <MentorCard key={mentor._id} mentor={mentor} />)}
-          </Segment.Group>
+          <div>
+            <classSubMenu/>
+          </div>
+
+          <Menu vertical>
+            <Menu.Item>
+              <Menu.Header>STEM</Menu.Header>
+              <Menu.Menu>
+                <Menu.Item
+                    name='Pysics'
+                    active={activeItem === 'Physics'}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                    name='Chemistry'
+                    active={activeItem === 'Chemistry'}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                    name='Biology'
+                    active={activeItem === 'Biology'}
+                    onClick={this.handleItemClick}
+                />
+                <Dropdown item text='Engineering'>
+                  <Dropdown.Menu>
+                    <Dropdown.Item text='Mechanical' />
+                    <Dropdown.Item text='Civil' />
+                    <Dropdown.Item text='Electrical' />
+                    <Dropdown.Item text='Computer' />
+                  </Dropdown.Menu>
+                </Dropdown>
+                <Menu.Item
+                    name='Math'
+                    active={activeItem === 'Math'}
+                    onClick={this.handleItemClick}
+                />
+              </Menu.Menu>
+            </Menu.Item>
+            <Menu.Item>
+              <Menu.Header>Social Sciences</Menu.Header>
+              <Menu.Menu>
+                <Menu.Item
+                    name='Psychology'
+                    active={activeItem === 'Psychology'}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                    name='Antrhopology'
+                    active={activeItem === 'Anthropology'}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                    name='Sociology'
+                    active={activeItem === 'Sociology'}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                    name='Political Science'
+                    active={activeItem === 'Political Science'}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                    name='Economics'
+                    active={activeItem === 'Economics'}
+                    onClick={this.handleItemClick}
+                />
+              </Menu.Menu>
+            </Menu.Item>
+          </Menu>
+
         </Container>
     );
   }

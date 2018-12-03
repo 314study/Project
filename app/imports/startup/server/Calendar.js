7af91c94b20db07ebda2bb35b57f054ed252f562
kainyogi <kainyogi@hawaii.edu>
2018-12-02 17:44:23 -1000

    Calendar fix

--- app/imports/startup/server/Calendar.js
+++ app/imports/startup/server/Calendar.js
@@ -1,10 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Calendar } from '../../api/Calendar/Calendar.js';
+import { Mentors } from '../../api/mentor/mentor';
 
 /** Initialize the database with a default data document. */
 function addData(data) {
-  console.log(`  Adding: ${data.timeBegin} ${data.timeEnd}`);
+  console.log(`  Adding: ${data.timeBegin} `);
   Calendar.insert(data);
 }
 
@@ -15,6 +16,10 @@ if (Calendar.find().count() === 0) {
     Meteor.settings.defaultCalendar.map(data => addData(data));
   }
 }
+
+Meteor.publish('Calendar', function publish() {
+  return Calendar.find();
+});
 
 /** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
 Meteor.publish('Calendar', function publish() {
